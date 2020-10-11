a32152999b28e9f9ae33527d2014b41952d8d73d
Sanyath Thammawong <sanyath007@gmail.com>
2020-10-11 15:58:28 +0700

    Use useState react hooks to show/hide sidebar menu

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -10,18 +10,18 @@ import {
   SidebarRoute
 } from './SidebarElements'
 
-const Sidebar = () => {
+const Sidebar = ({ isOpen, toggle }) => {
   return (
-    <SidebarContainer>
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
       <Icon>
         <CloseIcon />
       </Icon>
       <SidebarWrapper>
         <SidebarMenu>
-          <SidebarLink to='about'>About</SidebarLink>
-          <SidebarLink to='discover'>Discover</SidebarLink>
-          <SidebarLink to='services'>Services</SidebarLink>
-          <SidebarLink to='signup'>Sign Up</SidebarLink>
+          <SidebarLink to='about' onClick={toggle}>About</SidebarLink>
+          <SidebarLink to='discover' onClick={toggle}>Discover</SidebarLink>
+          <SidebarLink to='services' onClick={toggle}>Services</SidebarLink>
+          <SidebarLink to='signup' onClick={toggle}>Sign Up</SidebarLink>
         </SidebarMenu>
         <SideBtnWrap>
           <SidebarRoute to='signin'>Sign In</SidebarRoute>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar/Navbar'
 import Sidebar from '../components/Sidebar'
 
 const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <>
-      <Sidebar />
-      <Navbar />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
     </>
   )
 }
